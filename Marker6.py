2c6af7da600ed9ee87967edc05c87958c7f4fb3d
JacqKevin <jacq.kevin@hotmail.fr>
2020-09-12 23:15:21 +0200

    Create Marker6.py

---
+++ Marker6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import Metashape # Ouverture
+chunk = Metashape.app.document.chunk # Détection du dossier de travail 
+print("Detection des marqueurs")
+chunk.detectMarkers(Metashape.TargetType.CircularTarget12bit,100) # détection des marqueurs
+chunk.loadReference("C:/Users/edytem/Desktop/ASSEMBLAGES_PHOTOS_CAROTTES/MarkerBanc6.txt",Metashape.ReferenceFormatCSV) # Recherche des références
+chunk.updateTransform() # Sauvegarde
